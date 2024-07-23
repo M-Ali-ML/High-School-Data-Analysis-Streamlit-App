d3e60aaf6e8609bc8bf7ad6f9babfa2984860245
M. Ali <73258591+M-Ali-ML@users.noreply.github.com>
2024-07-23 08:20:33 +0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-@st.cache
+@st.cache_data
 def load_data(data_location):
     data = pd.read_csv(data_location, compression='zip')
     return data
